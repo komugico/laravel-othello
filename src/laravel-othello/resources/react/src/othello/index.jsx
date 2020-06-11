4435265697875a2ec43b99ff53106ad673caeb32
komugico <shu.isaka.pc+develop@gmail.com>
2020-06-11 22:00:24 +0900

    [機能修正]オセロのトップページを編集

--- src/laravel-othello/resources/react/src/othello/index.jsx
+++ src/laravel-othello/resources/react/src/othello/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Card, Col, Container, Row, ProgressBar } from 'react-bootstrap';
+import { Button,  Card, Col, Container, Row, Jumbotron } from 'react-bootstrap';
 
 import { getValueById } from '../share/finder.jsx';
 
@@ -15,6 +15,9 @@ class Index extends React.Component {
         }
 
         this.handleClick_btnViewGameLog = this.handleClick_btnViewGameLog.bind(this);
+        this.handleClick_btnCreateRoom = this.handleClick_btnCreateRoom.bind(this);
+        this.handleClick_btnJoinRoom = this.handleClick_btnJoinRoom.bind(this);
+        this.handleClick_btnWatchRoom = this.handleClick_btnWatchRoom.bind(this);
     }
 
     componentDidMount() {
@@ -36,12 +39,67 @@ class Index extends React.Component {
         alert("この機能は未実装です．");
     }
 
+    handleClick_btnCreateRoom() {
+        alert("この機能は未実装です．");
+    }
+
+    handleClick_btnJoinRoom() {
+        alert("この機能は未実装です．");
+    }
+
+    handleClick_btnWatchRoom() {
+        alert("この機能は未実装です．");
+    }
+
     render() {
         return (
             <Container>
-                <br />
                 <Row>
+                    <br />
                     <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                        <h1>Othello - Top</h1>
+                    </Col>
+                    <br />
+                    <Col xl={8} lg={8} md={8} sm={12} xs={12}>
+                        <Row>
+                            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                                <Jumbotron>
+                                    <h1>Create a room</h1>
+                                    <p>
+                                        You can build a new game.
+                                    </p>
+                                    <p>
+                                        <Button variant="primary" onClick={this.handleClick_btnCreateRoom}>Create</Button>
+                                    </p>
+                                </Jumbotron>
+                            </Col>
+                            <br />
+                            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                                <Jumbotron>
+                                    <h1>Join the room</h1>
+                                    <p>
+                                        You can fight with other players.
+                                    </p>
+                                    <p>
+                                        <Button variant="primary" onClick={this.handleClick_btnJoinRoom}>Join</Button>
+                                    </p>
+                                </Jumbotron>
+                            </Col>
+                            <br />
+                            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+                                <Jumbotron>
+                                    <h1>Watch the game</h1>
+                                    <p>
+                                        You can watch a existing game.
+                                    </p>
+                                    <p>
+                                        <Button variant="primary" onClick={this.handleClick_btnWatchRoom}>Watch</Button>
+                                    </p>
+                                </Jumbotron>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xl={4} lg={4} md={4} sm={12} xs={12}>
                         <Card>
                             <Card.Header as="h5">Player Information</Card.Header>
                             <Card.Body>
